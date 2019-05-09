3c65388fb0221aaa32c83d41e288de5aeb3b26f5
shiincs <skywing27@gmail.com>
2019-05-09 22:44:31 +0900

    feat: add map container component

---
+++ src/containers/MapContainer.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+import Map from '../components/Map/Map';
+
+import {
+  getVisibleList,
+  getIsFetching,
+  getErrorMessage,
+  fetchList,
+} from '../ducks/list';
+
+class MapContainer extends Component {
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.category !== prevProps.category) {
+      this.fetchData();
+    }
+  }
+
+  fetchData() {
+    const { category, fetchList } = this.props;
+    console.log(category);
+    fetchList(category).then(() => console.log('map done!'));
+  }
+
+  render() {
+    const { list } = this.props;
+    console.log(list);
+    return <Map list={list} />;
+  }
+}
+
+const mapStateToProps = (
+  state,
+  {
+    match: {
+      params: { category },
+    },
+  },
+) => {
+  const selectedCategory = category || 'all';
+  return {
+    list: getVisibleList(state, selectedCategory),
+    isFetching: getIsFetching(state, selectedCategory),
+    category: selectedCategory,
+    errorMessage: getErrorMessage(state, selectedCategory),
+  };
+};
+
+// MapContainer = withRouter(
+//   connect(
+//     mapStateToProps,
+//     { fetchList },
+//   )(MapContainer),
+// );
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { fetchList },
+  )(MapContainer),
+);
