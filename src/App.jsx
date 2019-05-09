0a75775d78619d2298c20d21fdf179253d88ce16
shiincs <skywing27@gmail.com>
2019-05-09 22:40:27 +0900

    feat: add redux provider & map context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import ProfileContainer from './containers/ProfileContainer';
 import AddPlaceContainer from './containers/AddPlaceContainer';
 import MainContainer from './containers/MainContainer';
+import { MapProvider } from './contexts/MapContext';
+import store from './store';
 
 function App() {
   return (
-    <Router>
-      <Route path="/profile" component={ProfileContainer} />
-      <Route path="/addplace" component={AddPlaceContainer} />
-      <Route exact path="/" component={MainContainer} />
-    </Router>
+    <Provider store={store}>
+      <MapProvider>
+        <Router>
+          <Route path="/profile" component={ProfileContainer} />
+          <Route path="/addplace" component={AddPlaceContainer} />
+          <Route path="/:category" component={MainContainer} />
+          <Route exact path="/" component={MainContainer} />
+        </Router>
+      </MapProvider>
+    </Provider>
   );
 }
 
