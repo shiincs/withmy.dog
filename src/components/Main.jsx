02851555229295d8ac024f14273f2311ae35c76a
shiincs <skywing27@gmail.com>
2019-05-10 18:23:49 +0900

    feat: add listOpenBtn & StorListContainer

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,13 +3,17 @@ import React from 'react';
 import Header from './Header';
 import StoreType from './StoreType';
 import MapContainer from '../containers/MapContainer';
+import ListOpenBtn from './ListOpenBtn';
+import StoreListContainer from '../containers/StoreListContainer';
 
-const Main = () => {
+const Main = props => {
   return (
     <>
       <Header />
       <StoreType />
       <MapContainer />
+      <ListOpenBtn {...props} />
+      <StoreListContainer {...props} />
     </>
   );
 };
