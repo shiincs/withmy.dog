e472ac600bd43ea057b7c0ad1b6c08dda22c7eb9
shiincs <skywing27@gmail.com>
2019-05-28 21:20:51 +0900

    feat: add AddPlaceForm components

---
+++ src/components/AddPlaceForm.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function AddPlaceForm(props) {
+  return <div>폼</div>;
+}
---
+++ src/containers/AddPlaceFormContainer.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import AddPlaceForm from '../components/AddPlaceForm';
+
+export default class AddPlaceFormContainer extends Component {
+  render() {
+    return <AddPlaceForm />;
+  }
+}
