6a68871e088c096a55261a70cd79ce5708f45422
shiincs <skywing27@gmail.com>
2019-05-30 17:38:28 +0900

    feat: add register form components

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const RegisterForm = () => {
+  return (
+    <>
+      <Form>
+        <InputID type="text" placeholder="이메일 입력" />
+        <InputPW type="password" placeholder="비밀번호 입력" />
+        <InputSubmit type="submit" value="계정 생성" />
+      </Form>
+      <GoToLogin>
+        <span>이미 계정이 있으신가요? </span>
+        <Link to="/profile/login">로그인</Link>
+      </GoToLogin>
+    </>
+  );
+};
+
+export default RegisterForm;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const InputID = styled.input`
+  width: 210px;
+  height: 45px;
+  background-color: #e0e0e0;
+  padding: 12px 17px;
+  font-size: 14px;
+  margin-bottom: 10px;
+  font-weight: 600;
+`;
+
+const InputPW = styled.input`
+  width: 210px;
+  height: 45px;
+  background-color: #e0e0e0;
+  padding: 12px 17px;
+  font-size: 14px;
+  margin-bottom: 10px;
+  font-weight: 600;
+`;
+
+const InputSubmit = styled.input`
+  width: 210px;
+  height: 45px;
+  background-color: #63bc69;
+  color: #fff;
+  padding: 12px 17px;
+  font-size: 14px;
+  font-weight: 600;
+`;
+
+const GoToLogin = styled.div`
+  text-align: center;
+  color: #828282;
+  font-weight: 600;
+  margin-top: 5px;
+
+  & > a:link,
+  & > a:visited,
+  & > a:active {
+    color: #63bc69;
+  }
+`;
---
+++ src/containers/RegisterFormContainer.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import RegisterForm from '../components/RegisterForm';
+
+export default class RegisterFormContainer extends Component {
+  render() {
+    return <RegisterForm />;
+  }
+}
