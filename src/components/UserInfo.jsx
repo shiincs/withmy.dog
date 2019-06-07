6b9f4ef210dbaedb8e4fffb8452f007fdb3d0d7e
shiincs <skywing27@gmail.com>
2019-06-07 10:09:16 +0900

    feat: add UserInfo components

---
+++ src/components/UserInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PersonalInfoContainer from '../containers/PersonalInfoContainer';
+import ActivityInfoContainer from '../containers/ActivityInfoContainer';
+
+const UserInfo = () => {
+  return (
+    <>
+      <PersonalInfoContainer />
+      <ActivityInfoContainer />
+    </>
+  );
+};
+
+export default UserInfo;
---
+++ src/containers/UserInfoContainer.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import UserInfo from '../components/UserInfo';
+
+export default class UserInfoContainer extends Component {
+  render() {
+    return <UserInfo />;
+  }
+}
