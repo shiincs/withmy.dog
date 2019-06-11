2a58ccc0e0b3207c1478b60065d2b4d0f3ebe743
shiincs <skywing27@gmail.com>
2019-06-11 22:59:07 +0900

    fix: edit createStore to use redux-devtools-extension

--- src/store/index.js
+++ src/store/index.js
@@ -4,19 +4,19 @@ import {
 } from 'redux';
 import thunk from 'redux-thunk';
 /* for dev */
-// import {
-//   composeWithDevTools
-// } from 'redux-devtools-extension';
+import {
+  composeWithDevTools
+} from 'redux-devtools-extension';
 
 import rootReducer from '../ducks';
 
 const middlewares = [thunk];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+// const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 /* for dev */
-// const store = createStore(rootReducer, composeWithDevTools(
-//   applyMiddleware(...middlewares)
-// ));
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(...middlewares)
+));
 
 export default store;
