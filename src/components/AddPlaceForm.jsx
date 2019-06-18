2844993d98bbb783b482d70e1b18e21c10134180
shiincs <skywing27@gmail.com>
2019-06-18 23:50:34 +0900

    fix: seperate form contents (only place category select)

--- src/components/AddPlaceForm.jsx
+++ src/components/AddPlaceForm.jsx
@@ -1,210 +1,79 @@
 import React from 'react';
 import styled from 'styled-components';
+import SectionHeader from './SectionHeader';
 
 export default function AddPlaceForm(props) {
-  const {
-    qs,
-    placeType,
-    dogType,
-    existType,
-    accessType,
-    contactType,
-    textInfo,
-    handleSelectInput,
-    handleCheckInput,
-    handleTextInput,
-  } = props;
+  const { qs, placeType, handleSelectInput, handleSubmit } = props;
   // TODO
   // 1. form input validation 디자인과 결합해서 로직 짜야함
   // 2. 사진 추가하기 버튼 레이아웃 및 file system 연동
   return (
-    <FormSection>
-      <PlaceInfo>
-        <PlaceName>{qs.name}</PlaceName>
-        <PlaceAddress>{qs.address}</PlaceAddress>
-      </PlaceInfo>
-      <Form>
-        <fieldset>
-          <Legend>가게 등록</Legend>
-          <QuestionWrapper>
-            <QuestionTitle>가게의 카테고리를 선택해주세요</QuestionTitle>
-            <Input
-              type="radio"
-              id="restaurant"
-              name="placeType"
-              checked={placeType === 'restaurant'}
-              onChange={() => handleSelectInput('placeType', 'restaurant')}
-              required
-            />
-            <PlaceLabel htmlFor="restaurant">식당</PlaceLabel>
-            <Input
-              type="radio"
-              id="cafe"
-              name="placeType"
-              checked={placeType === 'cafe'}
-              onChange={() => handleSelectInput('placeType', 'cafe')}
-            />
-            <PlaceLabel htmlFor="cafe">카페</PlaceLabel>
-            <Input
-              type="radio"
-              id="pub"
-              name="placeType"
-              checked={placeType === 'pub'}
-              onChange={() => handleSelectInput('placeType', 'pub')}
-            />
-            <PlaceLabel htmlFor="pub">술집</PlaceLabel>
-            <Input
-              type="radio"
-              id="etc"
-              name="placeType"
-              checked={placeType === 'etc'}
-              onChange={() => handleSelectInput('placeType', 'etc')}
-            />
-            <PlaceLabel htmlFor="etc">기타</PlaceLabel>
-          </QuestionWrapper>
-          <QuestionWrapper>
-            <QuestionTitle>
-              가게에 함께 간 강아지는 어떤 아이인가요?
-            </QuestionTitle>
-            {/* checkbox validation은 required만으로는 안되서 따로 로직 짜야함 */}
-            <Input
-              type="checkbox"
-              id="small"
-              name="dogType"
-              checked={dogType.some(type => type === 'small')}
-              onChange={() => handleCheckInput('dogType', 'small')}
-            />
-            <Label htmlFor="small">소형견</Label>
-            <Input
-              type="checkbox"
-              id="big"
-              name="dogType"
-              checked={dogType.some(type => type === 'big')}
-              onChange={() => handleCheckInput('dogType', 'big')}
-            />
-            <Label htmlFor="big">대형견</Label>
-          </QuestionWrapper>
-          <QuestionWrapper>
-            <QuestionTitle>가게 안에서 강아지가 어떻게 있었나요?</QuestionTitle>
-            {/* checkbox validation은 required만으로는 안되서 따로 로직 짜야함 */}
-            <Input
-              type="checkbox"
-              id="offleash"
-              name="existType"
-              checked={existType.some(type => type === 'offleash')}
-              onChange={() => handleCheckInput('existType', 'offleash')}
-            />
-            <Label htmlFor="offleash">오프리쉬</Label>
-            <Input
-              type="checkbox"
-              id="seat"
-              name="existType"
-              checked={existType.some(type => type === 'seat')}
-              onChange={() => handleCheckInput('existType', 'seat')}
-            />
-            <Label htmlFor="seat">좌석</Label>
-            <Input
-              type="checkbox"
-              id="bag"
-              name="existType"
-              checked={existType.some(type => type === 'bag')}
-              onChange={() => handleCheckInput('existType', 'bag')}
-            />
-            <Label htmlFor="bag">가방</Label>
-            <Input
-              type="checkbox"
-              id="cage"
-              name="existType"
-              checked={existType.some(type => type === 'cage')}
-              onChange={() => handleCheckInput('existType', 'cage')}
-            />
-            <Label htmlFor="cage">케이지</Label>
-          </QuestionWrapper>
-          <QuestionWrapper>
-            <QuestionTitle>
-              가게의 어느 영역까지 강아지가 출입할 수 있었나요?
-            </QuestionTitle>
-            <Input
-              type="radio"
-              id="entire"
-              name="accessType"
-              checked={accessType === 'entire'}
-              onChange={() => handleSelectInput('accessType', 'entire')}
-              required
-            />
-            <Label htmlFor="entire">전체출입</Label>
-            <Input
-              type="radio"
-              id="partial"
-              name="accessType"
-              checked={accessType === 'partial'}
-              onChange={() => handleSelectInput('accessType', 'partial')}
-            />
-            <Label htmlFor="partial">부분출입</Label>
-            <Input
-              type="radio"
-              id="unknownAccess"
-              name="accessType"
-              checked={accessType === 'unknownAccess'}
-              onChange={() => handleSelectInput('accessType', 'unknownAccess')}
-            />
-            <Label htmlFor="unknownAccess">모르겠어요</Label>
-          </QuestionWrapper>
-          <QuestionWrapper>
-            <QuestionTitle>
-              강아지와 방문하기 전에 미리 연락해야 하나요?
-            </QuestionTitle>
-            <Input
-              type="radio"
-              id="needContact"
-              name="contactType"
-              checked={contactType === 'needContact'}
-              onChange={() => handleSelectInput('contactType', 'needContact')}
-              required
-            />
-            <Label htmlFor="needContact">방문전 연락 필요</Label>
-            <Input
-              type="radio"
-              id="noContact"
-              name="contactType"
-              checked={contactType === 'noContact'}
-              onChange={() => handleSelectInput('contactType', 'noContact')}
-            />
-            <Label htmlFor="noContact">필요없음</Label>
-            <Input
-              type="radio"
-              id="unknownContact"
-              name="contactType"
-              checked={contactType === 'unknownContact'}
-              onChange={() =>
-                handleSelectInput('contactType', 'unknownContact')
-              }
-            />
-            <Label htmlFor="unknownContact">모르겠어요</Label>
-          </QuestionWrapper>
-          <TextAreaWrapper>
-            <TextArea
-              id="textInfo"
-              placeholder="추가로 공유하고 싶은 내용이 있나요?"
-              value={textInfo}
-              onChange={e => handleTextInput(e)}
-            />
-            <WordCount>0 / 300</WordCount>
-          </TextAreaWrapper>
-          <Submit type="submit" value="등록하기" />
-        </fieldset>
-      </Form>
-    </FormSection>
+    <>
+      <FormSection>
+        <SectionHeader>독플 등록</SectionHeader>
+        <PlaceInfo>
+          <PlaceName>{qs.name}</PlaceName>
+          <PlaceAddress>{qs.address}</PlaceAddress>
+        </PlaceInfo>
+        <Form onSubmit={e => handleSubmit(e)}>
+          <fieldset>
+            <Legend>가게 등록</Legend>
+            <QuestionWrapper>
+              <QuestionTitle>가게의 카테고리를 선택해주세요</QuestionTitle>
+              <Input
+                type="radio"
+                id="restaurant"
+                name="placeType"
+                checked={placeType === 'restaurant'}
+                onChange={() => handleSelectInput('placeType', 'restaurant')}
+                required
+              />
+              <PlaceLabel htmlFor="restaurant">식당</PlaceLabel>
+              <Input
+                type="radio"
+                id="cafe"
+                name="placeType"
+                checked={placeType === 'cafe'}
+                onChange={() => handleSelectInput('placeType', 'cafe')}
+              />
+              <PlaceLabel htmlFor="cafe">카페</PlaceLabel>
+              <Input
+                type="radio"
+                id="pub"
+                name="placeType"
+                checked={placeType === 'pub'}
+                onChange={() => handleSelectInput('placeType', 'pub')}
+              />
+              <PlaceLabel htmlFor="pub">술집</PlaceLabel>
+              <Input
+                type="radio"
+                id="etc"
+                name="placeType"
+                checked={placeType === 'etc'}
+                onChange={() => handleSelectInput('placeType', 'etc')}
+              />
+              <PlaceLabel htmlFor="etc">기타</PlaceLabel>
+            </QuestionWrapper>
+            <Submit type="submit" value="등록하기" />
+          </fieldset>
+        </Form>
+      </FormSection>
+    </>
   );
 }
 
 const FormSection = styled.section`
   position: relative;
-  padding: 15px;
+  height: 100vh;
 `;
 const PlaceInfo = styled.div`
-  padding: 0 0 15px 0;
+  height: 80px;
+  padding: 20px 0;
+  margin: 0 15px;
   border-bottom: 1px solid #e0e4e6;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 const PlaceName = styled.span`
   font-size: 16px;
@@ -219,7 +88,7 @@ const PlaceAddress = styled.span`
   line-height: 20px;
 `;
 const Form = styled.form`
-  padding: 20px 0 0 0;
+  padding: 20px 15px 0;
 `;
 const Legend = styled.legend`
   position: absolute;
@@ -254,21 +123,6 @@ const PlaceLabel = styled.label`
     color: #fff;
   }
 `;
-const Label = styled.label`
-  border: 2px solid #eaeeef;
-  color: #878d91;
-  padding: 7px 13px;
-  border-radius: 25px;
-  margin-right: 10px;
-  cursor: pointer;
-
-  input[type='radio']:checked + &,
-  input[type='checkbox']:checked + & {
-    background-color: #353a3c;
-    border: 2px solid #353a3c;
-    color: #fff;
-  }
-`;
 const Input = styled.input`
   position: absolute;
   width: 1px;
@@ -279,30 +133,10 @@ const Input = styled.input`
   clip: rect(0, 0, 0, 0);
   border: 0;
 `;
-const TextAreaWrapper = styled.div`
-  margin-bottom: 30px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-`;
-const TextArea = styled.textarea`
-  width: 100%;
-  height: 150px;
-  border: 2px solid #eaeeef;
-  border-radius: 10px;
-  padding: 15px 10px;
-  outline: none;
-
-  &:focus {
-    border-color: #01a462;
-  }
-`;
-const WordCount = styled.span`
-  color: #878d91;
-  font-size: 12px;
-`;
 const Submit = styled.input`
-  width: 100%;
+  position: absolute;
+  bottom: 15px;
+  width: calc(100% - 30px);
   height: 50px;
   border-radius: 25px;
   background-color: #01a462;
--- src/containers/AddPlaceFormContainer.jsx
+++ src/containers/AddPlaceFormContainer.jsx
@@ -2,15 +2,12 @@ import React, { Component } from 'react';
 import queryString from 'query-string';
 
 import AddPlaceForm from '../components/AddPlaceForm';
+import AddPlaceDone from '../components/AddPlaceDone';
 
 export default class AddPlaceFormContainer extends Component {
   state = {
     placeType: '',
-    dogType: [],
-    existType: [],
-    accessType: '',
-    contactType: '',
-    textInfo: '',
+    addPlaceDone: false,
   };
 
   handleSelectInput = (name, type) => {
@@ -19,42 +16,30 @@ export default class AddPlaceFormContainer extends Component {
     });
   };
 
-  handleCheckInput = (name, type) => {
-    const { dogType, existType } = this.state;
-    let prevState;
-    if (name === 'dogType') {
-      prevState = [...dogType];
-    } else if (name === 'existType') {
-      prevState = [...existType];
-    }
-    let newState = null;
-    if (prevState.some(item => item === type)) {
-      newState = prevState.filter(item => item !== type);
-    } else {
-      newState = [...prevState, type];
-    }
+  handleSubmit = e => {
+    e.preventDefault();
     this.setState({
-      [name]: newState,
-    });
-  };
-
-  handleTextInput = e => {
-    this.setState({
-      textInfo: e.target.value,
+      addPlaceDone: true,
     });
   };
 
   render() {
+    const { addPlaceDone } = this.state;
     const { location } = this.props;
     const qs = queryString.parse(location.search);
     return (
-      <AddPlaceForm
-        qs={qs}
-        handleSelectInput={this.handleSelectInput}
-        handleCheckInput={this.handleCheckInput}
-        handleTextInput={this.handleTextInput}
-        {...this.state}
-      />
+      <>
+        {addPlaceDone ? (
+          <AddPlaceDone qs={qs} />
+        ) : (
+          <AddPlaceForm
+            qs={qs}
+            handleSelectInput={this.handleSelectInput}
+            handleSubmit={this.handleSubmit}
+            {...this.state}
+          />
+        )}
+      </>
     );
   }
 }
